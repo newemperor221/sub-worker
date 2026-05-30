751de1368a30e1f088c5a99fa101031851f1da4f
M78星云皇帝 <1002464042@qq.com>
2026-05-30 08:50:58 -0400

    fix: 去掉 filename 后缀 .yaml/.txt

--- _worker.js
+++ _worker.js
@@ -482,7 +482,7 @@ async function handleRequest(request, env) {
     return new Response(yaml, {
       headers: {
         'Content-Type': 'text/yaml; charset=utf-8',
-        'content-disposition': 'inline; filename="' + config.subName + '.yaml"',
+        'content-disposition': 'inline; filename="' + config.subName + '"',
         'profile-title': config.subName,
         'profile-update-interval': '6',
       },
@@ -496,7 +496,7 @@ async function handleRequest(request, env) {
     return new Response(b64, {
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
-        'content-disposition': 'inline; filename="' + config.subName + '.txt"',
+        'content-disposition': 'inline; filename="' + config.subName + '"',
         'profile-title': config.subName,
         'profile-update-interval': '6',
       },
