2bd2d7f08dedcdc63aa91734dca3b1ca5b8c2bb3
M78星云皇帝 <1002464042@qq.com>
2026-05-30 08:53:23 -0400

    fix: Content-Disposition 用 filename* UTF-8 格式支持中文

--- _worker.js
+++ _worker.js
@@ -482,7 +482,7 @@ async function handleRequest(request, env) {
     return new Response(yaml, {
       headers: {
         'Content-Type': 'text/yaml; charset=utf-8',
-        'content-disposition': 'inline; filename="' + config.subName + '"',
+        'content-disposition': 'inline; filename="' + config.subName + '"; filename*=UTF-8\'\'' + encodeURIComponent(config.subName),
         'profile-title': config.subName,
         'profile-update-interval': '6',
       },
@@ -496,7 +496,7 @@ async function handleRequest(request, env) {
     return new Response(b64, {
       headers: {
         'Content-Type': 'text/plain; charset=utf-8',
-        'content-disposition': 'inline; filename="' + config.subName + '"',
+        'content-disposition': 'inline; filename="' + config.subName + '"; filename*=UTF-8\'\'' + encodeURIComponent(config.subName),
         'profile-title': config.subName,
         'profile-update-interval': '6',
       },
