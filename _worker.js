5937b9fd6906b370ef2b172292b47467fb72001c
M78星云皇帝 <1002464042@qq.com>
2026-06-23 20:02:49 -0400

    revert: remove Subscription-Userinfo header, Shadowrocket name comes from 备注 field

--- _worker.js
+++ _worker.js
@@ -43,7 +43,6 @@ async function handleRequest(request, env) {
         'content-disposition': 'inline; filename="' + config.subName + '"; filename*=UTF-8\'\'' + encodeURIComponent(config.subName),
         'profile-title': config.subName,
         'profile-update-interval': '6',
-        'Subscription-Userinfo': 'upload=0; download=0; total=1099511627776; expire=0',
       },
     });
   }
@@ -61,7 +60,6 @@ async function handleRequest(request, env) {
         'Content-Type': 'text/plain; charset=utf-8',
         'profile-title': config.subName,
         'profile-update-interval': '6',
-        'Subscription-Userinfo': 'upload=0; download=0; total=1099511627776; expire=0',
       },
     });
   }
